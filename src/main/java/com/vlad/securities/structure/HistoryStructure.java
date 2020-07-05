017ada8421b176892edd6f7565b69315ee6af6dc
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2020-07-06 02:45:13 +0300

    Create structures for history and securities.
    Structures describe properties for entity. It will help for xml parsing.

---
+++ src/main/java/com/vlad/securities/structure/HistoryStructure.java
@@ -0,0 +1,27 @@
+package com.vlad.securities.structure;
+
+public class HistoryStructure {
+
+    public static final String
+            BOARDID = "BOARDID",
+            TRADEDATE = "TRADEDATE",
+            SHORTNAME = "SHORTNAME",
+            SECID = "SECID",
+            NUMTRADES = "NUMTRADES",
+            VALUE = "VALUE",
+            OPEN = "OPEN",
+            LOW = "LOW",
+            HIGH = "HIGH",
+            LEGALCLOSEPRICE = "LEGALCLOSEPRICE",
+            WAPRICE = "WAPRICE",
+            CLOSE = "CLOSE",
+            VOLUME = "VOLUME",
+            MARKETPRICE2 = "MARKETPRICE2",
+            MARKETPRICE3 = "MARKETPRICE3",
+            ADMITTEDQUOTE = "ADMITTEDQUOTE",
+            MP2VALTRD = "MP2VALTRD",
+            MARKETPRICE3TRADESVALUE = "MARKETPRICE3TRADESVALUE",
+            ADMITTEDVALUE = "ADMITTEDVALUE",
+            WAVA = "WAVA";
+
+}
---
+++ src/main/java/com/vlad/securities/structure/SecurityPaperStructure.java
@@ -0,0 +1,23 @@
+package com.vlad.securities.structure;
+
+public class SecurityPaperStructure {
+
+    public static final String
+            id = "id",
+            secId = "secid",
+            shortName = "shortname",
+            regNumber = "regnumber",
+            name = "name",
+            isin = "isin",
+            isTraded = "is_traded",
+            emitentId = "emitent_id",
+            emitentTitle = "emitent_title",
+            emitentInn = "emitent_inn",
+            emitentOkpo = "emitent_okpo",
+            gosReg = "gosreg",
+            type = "type",
+            group = "group",
+            primaryBoardId = "primary_boardid",
+            marketPriceBoardId = "marketprice_boardid";
+
+}
