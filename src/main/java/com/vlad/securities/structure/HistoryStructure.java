82e2c42fd745561b96a99569690fd95a3796e5a4
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2020-07-06 22:24:42 +0300

    Add correctly names for structures

--- src/main/java/com/vlad/securities/structure/HistoryStructure.java
+++ src/main/java/com/vlad/securities/structure/HistoryStructure.java
@@ -3,25 +3,25 @@ package com.vlad.securities.structure;
 public class HistoryStructure {
 
     public static final String
-            BOARDID = "BOARDID",
-            TRADEDATE = "TRADEDATE",
-            SHORTNAME = "SHORTNAME",
-            SECID = "SECID",
-            NUMTRADES = "NUMTRADES",
+            BOARD_ID = "BOARDID",
+            TRADE_DATE = "TRADEDATE",
+            SHORT_NAME = "SHORTNAME",
+            SEC_ID = "SECID",
+            NUM_TRADES = "NUMTRADES",
             VALUE = "VALUE",
             OPEN = "OPEN",
             LOW = "LOW",
             HIGH = "HIGH",
-            LEGALCLOSEPRICE = "LEGALCLOSEPRICE",
-            WAPRICE = "WAPRICE",
+            LEGAL_CLOSE_PRICE = "LEGALCLOSEPRICE",
+            WA_PRICE = "WAPRICE",
             CLOSE = "CLOSE",
             VOLUME = "VOLUME",
-            MARKETPRICE2 = "MARKETPRICE2",
-            MARKETPRICE3 = "MARKETPRICE3",
-            ADMITTEDQUOTE = "ADMITTEDQUOTE",
-            MP2VALTRD = "MP2VALTRD",
-            MARKETPRICE3TRADESVALUE = "MARKETPRICE3TRADESVALUE",
-            ADMITTEDVALUE = "ADMITTEDVALUE",
-            WAVA = "WAVA";
+            MARKET_PRICE2 = "MARKETPRICE2",
+            MARKET_PRICE3 = "MARKETPRICE3",
+            ADMITTED_QUOTE = "ADMITTEDQUOTE",
+            MP2_VAL_TRD = "MP2VALTRD",
+            MARKET_PRICE3_TRADES_VALUE = "MARKETPRICE3TRADESVALUE",
+            ADMITTED_VALUE = "ADMITTEDVALUE",
+            WA_VA = "WAVA";
 
 }
--- src/main/java/com/vlad/securities/structure/SecurityPaperStructure.java
+++ src/main/java/com/vlad/securities/structure/SecurityPaperStructure.java
@@ -3,21 +3,21 @@ package com.vlad.securities.structure;
 public class SecurityPaperStructure {
 
     public static final String
-            id = "id",
-            secId = "secid",
-            shortName = "shortname",
-            regNumber = "regnumber",
-            name = "name",
-            isin = "isin",
-            isTraded = "is_traded",
-            emitentId = "emitent_id",
-            emitentTitle = "emitent_title",
-            emitentInn = "emitent_inn",
-            emitentOkpo = "emitent_okpo",
-            gosReg = "gosreg",
-            type = "type",
-            group = "group",
-            primaryBoardId = "primary_boardid",
-            marketPriceBoardId = "marketprice_boardid";
+            ID = "id",
+            SEC_ID = "secid",
+            SHORT_NAME = "shortname",
+            REG_NUMBER = "regnumber",
+            NAME = "name",
+            ISIN = "isin",
+            IS_TRADED = "is_traded",
+            EMITENT_ID = "emitent_id",
+            EMITENT_TITLE = "emitent_title",
+            EMITENT_INN = "emitent_inn",
+            EMITENT_OKPO = "emitent_okpo",
+            GOS_REG = "gosreg",
+            TYPE = "type",
+            GROUP = "group",
+            PRIMARY_BOARD_ID = "primary_boardid",
+            MARKET_PRICE_BOARD_ID = "marketprice_boardid";
 
 }
